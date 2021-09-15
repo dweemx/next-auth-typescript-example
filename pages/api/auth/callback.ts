775f7b2c6096a093794a5c576a14b6a2a2853d9e
dweemx <mdewaegeneer@gmail.com>
2021-09-15 21:40:39 +0200

    [WIP] Fixes bug magic link with Outlook email when SafeLinks

---
+++ pages/api/auth/callback.ts
@@ -0,0 +1,18 @@
+import { NextApiResponse } from "next";
+
+import * as nextAuthServerRoutes from "next-auth/server/routes"
+
+/**
+ * Fixes: https://github.com/nextauthjs/next-auth/issues/1840
+ * This dynamic route won't have priority over ./callback.ts route (https://nextjs.org/docs/routing/dynamic-routes).
+ * 
+ * FIXME: req types are internals (not exported) to next-auth (https://github.com/nextauthjs/next-auth/blob/main/types/internals/index.d.ts)
+ */
+
+export default (req: any, res: NextApiResponse): NextApiResponse => {
+    const { provider, baseUrl } = req.options
+    if(provider.type === "email" && req.method === "HEAD") {
+        return res.redirect(baseUrl)
+    }
+    return nextAuthServerRoutes.callback(req, res)
+}
